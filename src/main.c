1a7229c9a8b0ae49c5d393dba8bf9eba7265b8a4
Krzysztof Kozik <yuuichirou@tlen.pl>
2012-03-31 00:13:26 +0200

    Add a welcome message.

--- src/main.c
+++ src/main.c
@@ -9,7 +9,18 @@ uint16_t lcd_status ;
 
 int main (void)
 {
+  lcd_init () ;
+  lcd_goto_xy (3, 1) ;
+  lcd_print ("TEMPERATURE") ;
+  lcd_goto_xy (6, 2) ;
+  lcd_print ("LOGGER") ;
+  _delay_ms (2000) ;
+  lcd_clear_display () ;
 
+  while (1)
+  {
+    
+  }
   return 0 ;
 }
 
